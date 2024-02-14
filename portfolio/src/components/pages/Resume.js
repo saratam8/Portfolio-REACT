052b4ab65fa4bfdc54ff455e913c111b65bf5522
unknown <stam296@gmail.com>
2024-02-14 14:11:17 -0600

    edit resume source

--- portfolio/src/components/pages/Resume.js
+++ portfolio/src/components/pages/Resume.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Resume() {
     return (
     <div className="container text-center">
-        <iframe className="embed-responsive-item" src="/resume/SaraTam-Resume.pdf" title="Sara Tam's Resume" width="800" height="1000"></iframe>
+        <iframe className="embed-responsive-item" src="./resume/SaraTam-Resume.pdf" title="Sara Tam's Resume" width="800" height="1000"></iframe>
     </div>
     );
 }
