ab2e18a9337a3f0ae59225bd683127c0e7ceffb5
unknown <stam296@gmail.com>
2024-02-15 23:24:00 -0600

    update profile image

--- portfolio/src/components/pages/About.js
+++ portfolio/src/components/pages/About.js
@@ -13,7 +13,7 @@ export default function About() {
         <div className="container">
             <h1>About Me</h1>
             <div className="row">
-                <img className="col img-fluid" style={styles.img} src='./images/profile.jpeg' alt="Profile Picture" />
+                <img className="col img-fluid" style={styles.img} src='./images/profile.jpg' alt="Profile Picture" />
                 <p className="col text-wrap">
                     I have a Bachelor's of Science in Electrical Engineering from University of Illinois at Chicago. I am currently an Electrical Engineer at Northrop Grumman with a variety of experience in Systems Engineering, Software and Hardware. 
 
